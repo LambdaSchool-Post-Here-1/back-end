5046276c87bdfbee79dc30c0801997eea9ffd4dd
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-26 17:22:26 -0500

    got a few tests working on back end development environment. Should create separate database for testing?

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,9 +2,9 @@ const server = require("./server")
 const request = require("supertest")
 
 describe('GET /', () =>{
-  // it('has process.env.DB_ENV as "testing"', ()=>{
-  //   expect (process.env.DB_ENV).toBe('testing')
-  // })
+  it('has process.env.DB_ENV as "testing"', ()=>{
+    expect (process.env.DB_ENV).toBe('testing')
+  })
 
   it('returns 200 OK', () =>{
     return request(server).get('/')
---
+++ posts/post-model.spec.js
@@ -0,0 +1,20 @@
+const db = require('../data/dbconfig')
+const posts = require("./post-model")
+
+// describe('posts model', () =>{
+//   beforeEach(async()=>{
+//     await db(posts).truncate();
+//   })
+// })
+
+describe('add function', ()=>{
+  it('inserts new post in db', async () =>{
+    let postnumber
+    postnumber = await db('posts')
+    expect(postnumber).toHaveLength(11)
+    await posts.add('posts', {title:"this", content:"is a test", author:'jimmy'})
+    postnumber = await db('posts')
+    expect(postnumber).toHaveLength(12)
+
+  });
+})
