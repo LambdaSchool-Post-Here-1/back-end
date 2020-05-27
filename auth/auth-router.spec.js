e0ed1d4a550b357b4a5439381bd2422d8a1d97d1
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-27 17:46:46 -0500

    more tests passing

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,3 +1,19 @@
 const supertest = require("supertest")
 const server = require('../api/server')
 
+
+describe('POST to /api/auth/register', ()=>{
+  it('should send register user', ()=>{
+    return supertest(server).post('/api/auth/register')
+    .send({username:"john", password:"what"})
+    .then(response =>{ 
+      console.log("response body",response.body)
+      expect(response.status).toBe(201 || 200)
+      expect(response.body.data).toHaveProperty("password") // object that requests authentication information
+      // expect(response.body) // object that requests authentication information
+
+    })
+  })
+
+
+})
