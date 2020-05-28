cef9057f83eb07be92fc2fca44e8ba8725c1da39
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-27 19:01:39 -0500

    8 tests passed 3 suites

--- api/server.spec.js
+++ api/server.spec.js
@@ -8,10 +8,9 @@ describe('GET /', () =>{
 
   it('returns 200 OK', () =>{
     return supertest(server).get('/')
-    .expect('Content-Type', /json/)
-    .expect('Content-Length', '23')
     .then(response =>{
       expect(response.status).toBe(200)
+      expect(response.body).toMatch(/Server up and running/)
     })
 
 
